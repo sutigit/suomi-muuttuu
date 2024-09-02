b79583dd9076ca6b5a76b42b5d3af544ab4a9a4e
sutigit <panyanilben@gmail.com>
2024-09-02 13:41:37 +0300

    add initial interactivity function

--- src/app/components/MapComponent.tsx
+++ src/app/components/MapComponent.tsx
@@ -1,5 +1,8 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+// My imports
+import { getRandomColor } from '../lib/utils';
 
 // ol imports
 import GeoJSON from 'ol/format/GeoJSON.js';
@@ -7,6 +10,9 @@ import Map from 'ol/Map.js';
 import VectorLayer from 'ol/layer/Vector.js';
 import VectorSource from 'ol/source/Vector.js';
 import View from 'ol/View.js';
+import Style from 'ol/style/Style.js';
+import Fill from 'ol/style/Fill.js';
+import RenderEvent from 'ol/render/Event';
 
 // geojson
 import geojson from '@/geojson/suomen_kunta_jako.json';
@@ -27,108 +33,91 @@ interface Feature {
         type: string;
         coordinates: number[][][][];
     }
-}   
+}
 
 export default function MapComponent() {
+    // Use refs for states so that they don't trigger re-renders to avoid losing ol components
+    const animating = useRef(false);
 
-    // const filtered_geojson = geojson.features.filter((feature: Feature) => {
-    //     return feature.properties.SEAWAREA === 0.0;
-    // });
+    const vectorSource = new VectorSource({
+        features: new GeoJSON().readFeatures(geojson),
+        attributions: '© <a href="https://www.maanmittauslaitos.fi/">Maanmittauslaitos</a>',
+    })
 
-    // geojson.features = filtered_geojson;
+    const vectorLayer = new VectorLayer({
+        source: vectorSource,
+    });
+
+    const view = new View({
+        center: [0, 0],
+        zoom: 1,
+    });
+
+    let map: Map;
 
     useEffect(() => {
-        const vectorSource = new VectorSource({
-            features: new GeoJSON().readFeatures(geojson)
-        })
-
-        const vectorLayer = new VectorLayer({
-            source: vectorSource,
-            style: {
-                'fill-color': ['string', ['get', 'COLOR'], 'coral'],
-                // 'stroke-color': 'white',
-                // 'stroke-width': 1,
-            },
-        });
-
-        const view = new View({
-            center: [0, 0],
-            zoom: 1,
-        });
-
-        const map = new Map({
+        map = new Map({
             layers: [vectorLayer],
             target: 'map',
             view: view,
         });
 
-        // Fit the country to the map
+        // Fit country to the map
         const extent = vectorSource.getExtent();
         view.fit(extent, { size: map.getSize(), padding: [80, 80, 80, 80] });
 
-        // Remove all interactions and controls
-        map.getInteractions().clear();
+        // Remove controls
         map.getControls().clear();
 
-
-        // SOME COOL INTERACTIVITY SHIT ----
-
-        // const featureOverlay = new VectorLayer({
-        //     source: new VectorSource(),
-        //     map: map,
-        //     style: {
-        //         'stroke-color': 'rgba(255, 255, 255, 0.7)',
-        //         'stroke-width': 2,
-        //     },
-        // });
-
-        // let highlight: any;
-        // const displayFeatureInfo = function (pixel: any) {
-        //     const feature = map.forEachFeatureAtPixel(pixel, function (feature) {
-        //         return feature;
-        //     });
-
-        //     const info = document.getElementById('info');
-
-        //     if (feature && info) {
-        //         info.innerHTML = feature.get('ECO_NAME') || '&nbsp;';
-        //     } else if (info) {
-        //         info.innerHTML = '&nbsp;';
-        //     }
-
-        //     if (highlight && feature !== highlight) {
-        //         if (highlight) {
-        //             const source = featureOverlay.getSource()
-        //             if (source) {
-        //                 source.removeFeature(highlight);
-        //             }
-        //         }
-        //         if (feature) {
-        //             const source = featureOverlay.getSource()
-        //             if (source) {
-        //                 source.addFeature(feature as any);
-        //             }
-        //         }
-        //         highlight = feature;
-        //     }
-        // };
-
-        // map.on('pointermove', function (evt) {
-        //     if (evt.dragging) {
-        //         return;
-        //     }
-        //     const pixel = map.getEventPixel(evt.originalEvent);
-        //     displayFeatureInfo(pixel);
-        // });
-
-        // map.on('click', function (evt) {
-        //     displayFeatureInfo(evt.pixel);
-        // });
+        // Initial fill
+        const features = vectorSource.getFeatures();
+        features.forEach((feature) => {
+            feature.setStyle(new Style({
+                fill: new Fill({ color: getRandomColor() }),
+            }));
+        });
 
         return () => map.setTarget(undefined);
     }, []);
 
+    function fillFeatures(event: RenderEvent) {
+        setTimeout(() => {
+            const features = vectorSource.getFeatures();
+            features.forEach((feature) => {
+                feature.setStyle(new Style({
+                    fill: new Fill({ color: getRandomColor() }),
+                }));
+            });
+            map.render();
+        }, 50)
+    }
+
+    function startAnimation() {
+        animating.current = true;
+        vectorLayer.on('postrender', fillFeatures);
+
+        // trigger postrender event by changing something: set style to null
+        vectorLayer.setStyle();
+    }
+
+    function stopAnimation() {
+        animating.current = false;
+        vectorLayer.un('postrender', fillFeatures);
+        vectorLayer.setStyle(new Style({ fill: new Fill({ color: 'coral' }) }));
+    }
+
+    function animateMap() {
+        if (animating.current) {
+            stopAnimation();
+        } else {
+            startAnimation();
+        }
+    }
+
     return (
-        <div className='w-full h-full' id="map" />
+        <div className='w-full h-full'>
+            <button onClick={animateMap} className='bg-purple-500 text-white p-5'>Press me</button>
+            <div className='w-full h-full' id="map" />
+        </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,35 @@ import Map from './components/MapComponent';
 export default function App() {
 
   return (
-    <main className='bg-zinc-300 w-scree h-screen flex justify-center items-center'>
-      <section style={{width: 600, height: '100vh'}}>
+    <main className='bg-zinc-300 w-screen h-screen'>
+
+      {/* Background map */}
+      <section className='absolute inset-0 w-screen h-screen'>
         <Map />
       </section>
+
+      {/* MAP UI */}
+      <div className='flex justify-between p-16'>
+
+        {/* API selector */}
+        <section className='z-10 p-10 bg-blue-200 w-96 h-96'>
+          <button>Select</button>
+        </section>
+
+        {/* Editor */}
+        <section className='flex flex-col gap-10 z-10 w-96 h-96'>
+          {/* Player */}
+          <div className='p-10 bg-blue-200 w-full'>
+            <button className='bg-pink-600 p-5 text-white'>Play</button>
+          </div>
+
+          {/* Settings */}
+          <div className='p-10 bg-blue-200 w-full h-96'>
+            <p>Tarkkuus</p>
+          </div>
+        </section>
+
+      </div>
     </main>
   );
 }
