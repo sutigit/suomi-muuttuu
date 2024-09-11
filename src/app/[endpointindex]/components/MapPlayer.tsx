60f2bf31e44faf2d866cbff2838ab9d85359b67f
sutigit <panyanilben@gmail.com>
2024-09-11 14:11:19 +0300

    initial map player slider animation attempt

--- src/app/[endpointindex]/components/MapPlayer.tsx
+++ src/app/[endpointindex]/components/MapPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { MutableRefObject, useEffect, useRef } from 'react';
+import React, { MutableRefObject, useEffect, useState, useRef } from 'react';
 
 // shadcn
 import {
@@ -14,12 +14,19 @@ import {
 import PlayIcon from '@/svg-icons/play';
 import PauseIcon from '@/svg-icons/pause';
 import ToStartIcon from '@/svg-icons/to-start';
-import ToEndIcon from '@/svg-icons/to-end';
+import RepeatIcon from '@/svg-icons/repeat';
 import CaretDownIcon from '@/svg-icons/caret-down';
 
 
+
 // utils
-import { interpolateNumToRGB, natcodeToMetric, hexToRGB } from '../../lib/utils';
+import {
+    interpolateNumToRGB,
+    natcodeToMetric,
+    hexToRGB,
+    getRange,
+    getDiffBetween,
+} from '../../lib/utils';
 
 // definitions
 import { StatData } from '../../lib/definitions';
@@ -32,6 +39,7 @@ import View from 'ol/View.js';
 import Style from 'ol/style/Style.js';
 import Fill from 'ol/style/Fill.js';
 import RenderEvent from 'ol/render/Event';
+
 
 
 export default function MapPlayer({
@@ -60,29 +68,53 @@ export default function MapPlayer({
     endColor: string,
 }) {
 
+    // Initial styling
+    const startColorRgb = hexToRGB(startColor);
+    const endColorRgb = hexToRGB(endColor);
+
+    // Handle player control states
+    const [localLeftSideYear, setLocalLeftSideMinYear] = useState<number>(statMinYear);
+    const [localRightSideYear, setLocalRightSideMaxYear] = useState<number>(statMaxYear);
+    const [currentYear, setCurrentYear] = useState<number>(statMinYear);
+    const [speed, setSpeed] = useState<number>(1);
+    const [sliderHandlePosition, setSliderHandlePosition] = useState<number>(0);
+
+    function handleSelectLeftSideYear(year: string) {
+        stopAnimation();
+        setLocalLeftSideMinYear(parseInt(year));
+        setCurrentYear(parseInt(year));
+    }
+
+    function handleSelectRightSideYear(year: string) {
+        stopAnimation();
+        setLocalRightSideMaxYear(parseInt(year));
+    }
+
+    function handleSelectSpeed(speed: string) {
+        stopAnimation();
+        setSpeed(parseFloat(speed));
+    }
+
+    // Handle animation flow
+    const [animating, setAnimating] = useState<boolean>(false);
+
     const lastTimeRef = useRef<number | null>(null);
     const currentYearRef = useRef<number>(statMinYear);
     const speedRef = useRef<number>(300);
 
-    const [animating, setAnimating] = React.useState<boolean>(false);
-    const startColorRgb = hexToRGB(startColor);
-    const endColorRgb = hexToRGB(endColor);
-
-
     function animateFeatures(event: RenderEvent) {
         const time = event.frameState?.time;
         if (!time || !lastTimeRef.current) {
-            console.error('No time');
+            console.error('Invalid time');
             return;
         }
 
         const elapsedTime = time - lastTimeRef.current;
 
-        if (currentYearRef.current <= statMaxYear) {
+        if (currentYearRef.current <= localRightSideYear) {
 
             if (elapsedTime > speedRef.current) {
 
-                console.log('Animating', currentYearRef.current);
                 const features = sourceRef.current?.getFeatures();
 
                 features?.forEach((feature) => {
@@ -104,6 +136,8 @@ export default function MapPlayer({
                 });
 
                 currentYearRef.current += 1;
+                setCurrentYear(currentYearRef.current);
+                setSliderHandlePosition(prev => prev + 100/getDiffBetween(localLeftSideYear, localRightSideYear, 40));
                 lastTimeRef.current = time;
                 mapRef.current?.render();
             }
@@ -117,9 +151,10 @@ export default function MapPlayer({
 
     function startAnimation() {
         setAnimating(true);
+        setSliderHandlePosition(0);
 
         lastTimeRef.current = Date.now();
-        currentYearRef.current = statMinYear;
+        currentYearRef.current = localLeftSideYear;
         layerRef.current?.on('postrender', animateFeatures);
         // trigger postrender event by changing something: set style to null
         layerRef.current?.setStyle();
@@ -136,74 +171,105 @@ export default function MapPlayer({
             {/* Time selector */}
             <div className='flex justify-between'>
 
-                <Select>
+                {/* Set Min Year */}
+                <Select defaultValue={localLeftSideYear.toString()} onValueChange={handleSelectLeftSideYear}>
                     <SelectTrigger className="w-24">
-                        <SelectValue placeholder="1987" />
+                        <SelectValue />
                     </SelectTrigger>
                     <SelectContent>
-                        <SelectItem value="1987">1997</SelectItem>
-                        <SelectItem value="1988">1988</SelectItem>
+                        {
+                            getRange(statMinYear, statMaxYear).map((year) => (
+                                <SelectItem key={year} value={year.toString()}>{year}</SelectItem>
+                            ))
+                        }
                     </SelectContent>
                 </Select>
 
+                {/* Display current year */}
                 <div className='flex flex-col items-center justify-center mt-2 text-sm'>
-                    <span>2012</span>
+                    <span>{currentYear}</span>
                     <CaretDownIcon width={16} height={16} />
                 </div>
 
-                <Select>
+                {/* Set Max Year */}
+                <Select defaultValue={localRightSideYear.toString()} onValueChange={handleSelectRightSideYear}>
                     <SelectTrigger className="w-24">
-                        <SelectValue placeholder="2022" />
+                        <SelectValue />
                     </SelectTrigger>
                     <SelectContent>
-                        <SelectItem value="2022">2022</SelectItem>
-                        <SelectItem value="2023">2023</SelectItem>
+                        {
+                            getRange(statMinYear, statMaxYear).map((year) => (
+                                <SelectItem key={year} value={year.toString()}>{year}</SelectItem>
+                            ))
+                        }
                     </SelectContent>
                 </Select>
             </div>
+
 
             {/* Time slider */}
-            <div className='relative flex items-center h-3 w-full  mt-2 mb-5 px-2'>
-                <div className='w-full h-1 bg-black rounded-sm' />
-                <div className='absolute top-0 left-0 w-3 h-full rounded border border-black bg-black' />
+            <div className='relative flex items-center h-6 w-full mt-3 mb-6'>
+
+                { /* Slider bar */}
+                <div className='w-full h-4 bg-zinc-200 flex'>
+
+                    {/* Slider tics */}
+                    {
+                        getRange(localLeftSideYear, localRightSideYear, 40).map((year) => (
+                            <div key={year} className='border-r-2 border-white h-full' style={{ width: `${(100.0 / getDiffBetween(localLeftSideYear, localRightSideYear, 40))}%` }} />
+                        ))
+                    }
+                </div>
+
+                {/* Slider handle */}
+                <div className='absolute top-0 w-1 h-full rounded bg-black' style={{left: `${sliderHandlePosition}%`}}/>
             </div>
 
+
+
             {/* Media player */}
-            <div className='flex gap-5 justify-center items-center'>
+            <div className='flex justify-between items-center'>
 
-                {/* Skip to left end */}
-                <button>
-                    <ToStartIcon width={16} height={16} />
-                </button>
-
-                {/* Play / Pause */}
-                {animating ?
-                    <button onClick={stopAnimation}>
-                        <PauseIcon width={24} height={24} />
-                    </button> :
-                    <button onClick={startAnimation}>
-                        <PlayIcon width={24} height={24} />
+                {/* Play controls */}
+                <div className='flex items-center gap-3'>
+                    {/* Start from beginning */}
+                    <button>
+                        <ToStartIcon width={16} height={16} />
                     </button>
-                }
 
-                {/* Skip to right end */}
-                <button>
-                    <ToEndIcon width={16} height={16} />
-                </button>
+                    {/* Play / Pause */}
+                    {animating ?
+                        <button onClick={stopAnimation}>
+                            <PauseIcon width={24} height={24} />
+                        </button> :
+                        <button onClick={startAnimation}>
+                            <PlayIcon width={24} height={24} />
+                        </button>
+                    }
+
+                    {/* Repeat toggle */}
+                    <button>
+                        <RepeatIcon width={14} height={14} />
+                    </button>
+                </div>
+
 
                 {/* Speed selector */}
-                <Select>
-                    <SelectTrigger className="w-24">
-                        <SelectValue placeholder="1 x" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value="0.5">0.5 x</SelectItem>
-                        <SelectItem value="0.75">0.75 x</SelectItem>
-                        <SelectItem value="1">1 x</SelectItem>
-                        <SelectItem value="1.25">1.25 x</SelectItem>
-                        <SelectItem value="1.5">1.5 x</SelectItem>
-                    </SelectContent>
-                </Select>
+                <div className='flex items-center gap-2'>
+                    <p className='text-sm'>Nopeus:</p>
+                    <Select defaultValue={speed.toString()} onValueChange={handleSelectSpeed}>
+                        <SelectTrigger className="w-20">
+                            <SelectValue />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {
+                                getRange(0.25, 2, Infinity, 0.25).map((speed) => (
+                                    <SelectItem key={speed} value={speed.toString()}>{speed}</SelectItem>
+                                ))
+                            }
+                        </SelectContent>
+                    </Select>
+                </div>
             </div>
         </section>
     );
